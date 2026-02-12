6cda0b0156b06cbf376df2ec272399d444c15874
kowshikaravind4@gmail.com <kowshikaravind4@gmail.com>
2026-02-12 21:21:55 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div className="app">
+        <h1>DASHBOARD</h1>
+      </div>
+      
+    </div>
+  );
+}
+export default App;
+
