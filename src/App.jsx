8e108c033be09021712cf96e91ea6e34338ae5df
kowshikaravind4@gmail.com <kowshikaravind4@gmail.com>
2026-02-15 12:38:36 +0530

    Started to get student details from the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,82 @@
-import React from 'react';
+import { useState } from "react";
 import './App.css';
 
-const App = () => {
-  return (
+function App() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const [formData, setFormData] = useState({
+    studentID: "",
+    studentName: "",
+    parentNumber: "",
+    studentClass: "",
+  });
+
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.name] : e.target.value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData({
+      studentID: "",
+      studentName: "",
+      parentNumber: "",
+      studentClass: "",
+    });
+    setIsFormVisible(false);
+  };
+
+  return(
     <div>
-      <div className="app">
-        <h1>DASHBOARD</h1>
-      </div>
-      
+      {!isFormVisible &&(
+      <button onClick={() => setIsFormVisible(true)}>
+        Add Student
+      </button>
+      )}
+      {isFormVisible &&(
+        <div>
+          <h3>Enter Student Details</h3>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                type = "text"
+                name = "studentID"
+                placeholder="Student ID"
+                value={formData.studentID}
+                onChange={handleChange}
+              />
+              <input
+                type = "text"
+                name = "studentName"
+                placeholder="Student Name"
+                value={formData.studentName}
+                onChange={handleChange}
+              /><input
+                type = "text"
+                name = "parentNumber"
+                placeholder="Parent Number"
+                value={formData.parentNumber}
+                onChange={handleChange}
+              /><input
+                type = "text"
+                name = "studentClass"
+                placeholder="Student Class"
+                value={formData.studentClass}
+                onChange={handleChange}
+              />
+            </div>
+            <button>Submit</button>
+            <button type="button" onClick={() => setIsFormVisible(false)}>
+              Cancel
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
 export default App;
-
